fd6aa51606c84b647a561370f57f1a9d9c7f223c
Emmeran Bordessoules <SESA569486@se.com>
2021-03-30 11:29:55 +0200

    fix mapper + style

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -18,13 +18,14 @@ export class Card {
     card.subtypes = data.subtypes;
     card.types = data.types;
     card.images = data.images;
+    card.price = 25;
     if (data?.tcgplayer) {
       // Get the key of all the types of card
-      const key = Object.keys(data?.tcgplayer?.prices)[0];
-      // Get the market price of the first type of card
-      card.price = data?.tcgplayer?.prices[key].market;
-    } else {
-      card.price = 25;
+      const key = Object.keys(data.tcgplayer.prices)[0];
+      if (data?.tcgplayer.prices[key]) {
+        // Get the market price of the first type of card
+        card.price = data.tcgplayer.prices[key].market;
+      }
     }
     return card;
   }
